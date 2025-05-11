189f840d4e535e28cac78a8c2667e6ae515bb881
llixtar <shelakov92@gmail.com>
2025-05-12 00:29:53 +0200

    Reorganizing the slider with prototypes

--- index.js
+++ index.js
@@ -1,3 +1,159 @@
+function Slider(images, container) {
+    this.images = images;
+    this.container = container;
+    this.current = 0;
+    this.isPaused = false;
+    this.singleImgSize = 200;
+    this.imgElements = [];
+    this.indicators = [];
+
+    this.init();
+}
+
+Slider.prototype.init = function () {
+    this.renderImages();
+    this.renderIndicators();
+    this.renderButtons();
+    this.enableKeyboard();
+    this.enableMouseSwipe();
+    this.enableTouchSwipe();
+    this.startAutoPlay();
+};
+
+Slider.prototype.renderImages = function () {
+    for (let i = 0; i < this.images.length; i++) {
+        const img = document.createElement('img');
+        img.src = this.images[i];
+        img.setAttribute('draggable', 'false');
+        this.container.appendChild(img);
+        this.imgElements.push(img);
+    }
+};
+
+Slider.prototype.renderIndicators = function () {
+    const indicatorContainer = document.createElement('div');
+    indicatorContainer.className = 'slider-indicator-container';
+
+    for (let i = 0; i < this.images.length; i++) {
+        const dot = document.createElement('div');
+        dot.className = 'slider-indicator';
+        if (i === 0) {
+            dot.classList.add('slider-indicator-active');
+        }
+        indicatorContainer.appendChild(dot);
+        this.indicators.push(dot);
+    }
+
+    document.body.appendChild(indicatorContainer);
+};
+
+Slider.prototype.renderButtons = function () {
+    const btnContainer = document.createElement('div');
+    btnContainer.className = 'button-container';
+
+    const thisSlider = this;
+
+    const prevBtn = document.createElement('button');
+    prevBtn.innerText = 'Prev';
+    prevBtn.onclick = function () {
+        thisSlider.showSlide(false);
+    };
+
+    const nextBtn = document.createElement('button');
+    nextBtn.innerText = 'Next';
+    nextBtn.onclick = function () {
+        thisSlider.showSlide(true);
+    };
+
+    const pauseBtn = document.createElement('button');
+    pauseBtn.innerText = 'Pause';
+    pauseBtn.onclick = function () {
+        thisSlider.isPaused = !thisSlider.isPaused;
+        pauseBtn.innerText = thisSlider.isPaused ? 'Start' : 'Pause';
+    };
+
+    btnContainer.appendChild(prevBtn);
+    btnContainer.appendChild(nextBtn);
+    btnContainer.appendChild(pauseBtn);
+
+    document.body.appendChild(btnContainer);
+};
+
+Slider.prototype.showSlide = function (forward) {
+    if (forward) {
+        this.current++;
+        if (this.current === this.images.length) {
+            this.current = 0;
+        }
+    } else {
+        this.current--;
+        if (this.current < 0) {
+            this.current = this.images.length - 1;
+        }
+    }
+
+    const offset = -this.current * this.singleImgSize;
+    this.imgElements[0].style.marginLeft = offset + 'px';
+
+    for (let i = 0; i < this.indicators.length; i++) {
+        this.indicators[i].classList.toggle('slider-indicator-active', i === this.current);
+    }
+};
+
+Slider.prototype.startAutoPlay = function () {
+    const thisSlider = this;
+    setInterval(function () {
+        if (!thisSlider.isPaused) {
+            thisSlider.showSlide(true);
+        }
+    }, 1000);
+};
+
+Slider.prototype.enableKeyboard = function () {
+    const thisSlider = this;
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'ArrowLeft') {
+            thisSlider.showSlide(false);
+        } else if (e.key === 'ArrowRight') {
+            thisSlider.showSlide(true);
+        }
+    });
+};
+
+Slider.prototype.enableMouseSwipe = function () {
+    const thisSlider = this;
+    let startX = 0;
+
+    this.container.addEventListener('mousedown', function (e) {
+        startX = e.pageX;
+    });
+
+    this.container.addEventListener('mouseup', function (e) {
+        const dx = e.pageX - startX;
+        thisSlider.showSlide(dx < 0);
+    });
+};
+
+Slider.prototype.enableTouchSwipe = function () {
+    const thisSlider = this;
+    let startX = 0;
+
+    this.container.addEventListener('touchstart', function (e) {
+        startX = e.touches[0].clientX;
+    });
+
+    this.container.addEventListener('touchend', function (e) {
+        const endX = e.changedTouches[0].clientX;
+        const dx = endX - startX;
+        thisSlider.showSlide(dx < 0);
+    });
+};
+
+
+const container = document.createElement('div');
+container.className = 'main-container';
+document.body.appendChild(container);
+
 const imgsSrc = [
     'https://picsum.photos/id/10/200/300',
     'https://picsum.photos/id/20/200/300',
@@ -12,166 +168,7 @@ const imgsSrc = [
     'https://picsum.photos/id/110/200/300',
     'https://picsum.photos/id/120/200/300',
     'https://picsum.photos/id/130/200/300',
-    'https://picsum.photos/id/140/200/300',
-
+    'https://picsum.photos/id/140/200/300'
 ];
 
-const singleImgSize = 200;
-const body = document.body;
-
-function createElement(tag, classNames) {
-    const el = document.createElement(tag);
-    for (let i = 0; i < classNames.length; i++) {
-        el.classList.add(classNames[i]);
-    }
-    return el;
-}
-
-function createImage(src) {
-    const img = document.createElement('img');
-    img.src = src;
-    img.setAttribute('draggable', 'false');
-    return img;
-}
-
-function getNextPosition(slidesCount) {
-    let current = 0;
-
-    return function (forward) {
-        if (typeof forward === 'undefined') {
-            forward = true;
-        }
-
-        if (forward) {
-            current++;
-            if (current === slidesCount) {
-                current = 0;
-            }
-        } else {
-            current--;
-            if (current < 0) {
-                current = slidesCount - 1;
-            }
-        }
-
-        return current;
-    };
-}
-
-
-function updateSlide(index) {
-    const firstImg = document.querySelector("img[src='https://picsum.photos/id/10/200/300']");
-    if (firstImg) {
-        firstImg.style.marginLeft = (-index * singleImgSize) + 'px';
-    }
-
-    const indicators = document.getElementsByClassName('slider-indicator');
-    for (let i = 0; i < indicators.length; i++) {
-        indicators[i].classList.toggle('slider-indicator-active', i === index);
-    }
-}
-
-
-function renderImages(sources, container) {
-    for (let i = 0; i < sources.length; i++) {
-        container.appendChild(createImage(sources[i]));
-    }
-}
-
-function createButton(text, onClick) {
-    const btn = document.createElement('button');
-    btn.innerText = text;
-    if (onClick) {
-        btn.addEventListener('click', onClick);
-    }
-    return btn;
-}
-
-function createSliderButtons(getNextSlide, btnContainer) {
-    const prevBtn = createButton('Prev', function () {
-        updateSlide(getNextSlide(false));
-    });
-    const nextBtn = createButton('Next', function () {
-        updateSlide(getNextSlide(true));
-    });
-    btnContainer.appendChild(prevBtn);
-    btnContainer.appendChild(nextBtn);
-}
-
-function createPlayPauseButton(btnContainer) {
-    const btn = createButton('Pause');
-    let isPaused = false;
-
-    btn.addEventListener('click', function () {
-        isPaused = !isPaused;
-        btn.innerText = isPaused ? 'Start' : 'Stop';
-    });
-
-    btnContainer.appendChild(btn);
-
-    return function () {
-        return !isPaused;
-    };
-}
-
-function createIndicators(count) {
-    const container = createElement('div', ['slider-indicator-container']);
-    for (let i = 0; i < count; i++) {
-        const indicator = createElement('div', ['slider-indicator']);
-        if (i === 0) {
-            indicator.classList.add('slider-indicator-active');
-        }
-        container.appendChild(indicator);
-    }
-    body.appendChild(container);
-}
-
-function addSwipe(element, getNextSlide) {
-    let startX = 0;
-
-    element.addEventListener('mousedown', function (e) {
-        startX = e.x;
-    });
-
-    element.addEventListener('mouseup', function (e) {
-        const dx = e.x - startX;
-
-        updateSlide(getNextSlide(dx > 0));
-
-    });
-}
-
-function initializeSlider() {
-    const main = createElement('div', ['main-container']);
-    body.appendChild(main);
-
-    renderImages(imgsSrc, main);
-
-    const getNextSlide = getNextPosition(imgsSrc.length);
-
-    createIndicators(imgsSrc.length);
-
-    const buttonContainer = createElement('div', ['button-container']);
-    body.appendChild(buttonContainer);
-    createSliderButtons(getNextSlide, buttonContainer);
-
-    const isAutoPlay = createPlayPauseButton(buttonContainer);
-
-    setInterval(function () {
-        if (isAutoPlay()) {
-            updateSlide(getNextSlide(true));
-        }
-    }, 1000);
-
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'ArrowLeft') {
-            updateSlide(getNextSlide(false));
-        } else if (e.key === 'ArrowRight') {
-            updateSlide(getNextSlide(true));
-        }
-    });
-
-    addSwipe(main, getNextSlide);
-}
-
-initializeSlider();
+new Slider(imgsSrc, container);
